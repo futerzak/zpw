abb5fe960770ebb144296eee7b17b25dae056934
Michał Topór-Futer <michal.topor-futer@wiosna.org.pl>
2016-11-13 20:55:46 +0100

    temp

--- lab4/src/js/controllers.js
+++ lab4/src/js/controllers.js
@@ -1,7 +1,9 @@
 //var portfolioApp = angular.module('portfolioApp',[]);
 var appControllers = angular.module('appControllers',[]);
 
-appControllers.controller('GalleryListCtrl', function($scope, $http) {
+appControllers.controller('GalleryListCtrl', ['$scope', '$http', 'Gallery', function($scope, $http, Gallery) {
+
+    $scope.galleries = Gallery.query();
 
 	$scope.galleries = [];
 	$scope.sortList = [];
@@ -21,27 +23,35 @@ appControllers.controller('GalleryListCtrl', function($scope, $http) {
 	$scope.title = "Moja galeria zdjęć";
 	$scope.query = '';
 
-});
+}]);
 
-appControllers.controller('GalleryDetailCtrl', function($scope, $http, $routeParams) {
-
-    // $scope.gallery = {};
-
-    $http
-		.get("photos/"+$routeParams.galleryId+".json")
-        .then(function(response){
-            $scope.gallery = response.data.gallery;
-            $scope.title = "Moje podróże: " + $scope.gallery.title;
-            $scope.mainPhotoUrl = $scope.gallery.photos[0].photoUrl;
-            $scope.setPhoto = function(photo) {
-                $scope.mainPhotoUrl = photo.photoUrl;
-                console.log("set photo: "+photo.photoTitle)
-            }
-        },
-        function(errResponse) {
-            console.log('Error response', errResponse);
-        });
+appControllers.controller(
+    'GalleryDetailCtrl',
+    [
+        '$scope',
+        '$http',
+        '$routeParams',
+        'Gallery',
+        function($scope, $http, $routeParams, Gallery) {
 
 
-    $scope.galleryId = $routeParams.galleryId;
-});
+        $http
+    		.get("photos/"+$routeParams.galleryId+".json")
+            .then(function(response){
+                $scope.gallery = response.data.gallery;
+                $scope.title = "Moje podróże: " + $scope.gallery.title;
+                $scope.mainPhotoUrl = $scope.gallery.photos[0].photoUrl;
+                $scope.setPhoto = function(photo) {
+                    $scope.mainPhotoUrl = photo.photoUrl;
+                    console.log("set photo: "+photo.photoTitle)
+                }
+            },
+            function(errResponse) {
+                console.log('Error response', errResponse);
+            });
+
+
+        $scope.galleryId = $routeParams.galleryId;
+        }
+    ]
+);
